92ba90ec5a164fb83e600aa32f61e727b59c342c
akj_ubuntu <akjanto@gmail.com>
2021-01-26 14:03:23 +0700

    ff

--- app/app/main.py
+++ app/app/main.py
@@ -1,10 +1,9 @@
+from .core import app_setup
 from flask import Flask
 
 app = Flask(__name__)
 
-from .core import app_setup
-
 
 if __name__ == "__main__":
     # Only for debugging while developing
-    app.run(host="0.0.0.0", debug=True, port=8080)
+    app.run(host="0.0.0.0", debug=True, port=80)
