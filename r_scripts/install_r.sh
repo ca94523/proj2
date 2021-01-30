8dc0f597f2a1b747444f279c1da37aad36dba530
akj_ubuntu <akjanto@gmail.com>
2021-01-30 13:17:45 +0700

    dfd

--- r_scripts/install_r.sh
+++ r_scripts/install_r.sh
@@ -18,20 +18,18 @@ apt-get -y install --no-install-recommends \
 apt-key adv --keyserver keys.gnupg.net --recv-key 'E19F5F87128899B192B1A2C2AD5F960A256A04AF'
 add-apt-repository 'deb http://cloud.r-project.org/bin/linux/debian buster-cran40/'
 apt-get update && apt-get -y install --no-install-recommends r-base r-base-dev
-
+#default install to /usr/lib/R
 rm -rf /var/lib/apt/lists/*
 
-
 echo "options(repos = c(CRAN = 'https://packagemanager.rstudio.com/all/__linux__/bionic/latest'), download.file.method = 'libcurl')" >> /usr/lib/R/etc/Rprofile.site
-Rscript -e "options(HTTPUserAgent = sprintf('R/%s R (%s)', getRversion(), paste(getRversion(), R.version$platform, R.version$arch, R.version$os)))"
-Rscript -e 'options(download.file.extra = sprintf("--header \"User-Agent: R (%s)\"", paste(getRversion(), R.version$platform, R.version$arch, R.version$os)))'
+echo "options(HTTPUserAgent = sprintf('R/%s R (%s)', getRversion(), paste(getRversion(), R.version$platform, R.version$arch, R.version$os)))" >> /usr/lib/R/etc/Rprofile.site
 Rscript -e "install.packages(c('littler', 'docopt'), repos='https://packagemanager.rstudio.com/all/__linux__/bionic/latest')"
 
 ## By default R_LIBS_SITE is unset, and defaults to this, so this is where `littler` will be.
 ## We set it here for symlinks, but don't make the env var persist (since it's already the default)
-R_LIBS_SITE=/usr/local/lib/R/site-library
-ln -s ${R_LIBS_SITE}/littler/examples/install.r /usr/local/bin/install.r
-ln -s ${R_LIBS_SITE}/littler/examples/install2.r /usr/local/bin/install2.r
-ln -s ${R_LIBS_SITE}/littler/examples/installGithub.r /usr/local/bin/installGithub.r
-ln -s ${R_LIBS_SITE}/littler/bin/r /usr/local/bin/r
+#R_LIBS_SITE=/usr/local/lib/R/site-library
+#ln -s ${R_LIBS_SITE}/littler/examples/install.r /usr/local/bin/install.r
+#ln -s ${R_LIBS_SITE}/littler/examples/install2.r /usr/local/bin/install2.r
+#ln -s ${R_LIBS_SITE}/littler/examples/installGithub.r /usr/local/bin/installGithub.r
+#ln -s ${R_LIBS_SITE}/littler/bin/r /usr/local/bin/r
 
